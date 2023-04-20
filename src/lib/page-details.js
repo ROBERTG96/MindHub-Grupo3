087661ed0d22916ac9fc3fbdfaf91438c731cc2e
ROBERTG96 <71914244+ROBERTG96@users.noreply.github.com>
2023-04-20 02:08:35 -0500

    Important Changes - Settings

--- src/lib/page-details.js
+++ src/lib/page-details.js
@@ -9,14 +9,15 @@ function CargarDetails() {
 
     CardImg.innerHTML = `
     <div class='card_details' >
-    <img src="${detail.image}" class='card_details' alt="${detail.name}"></img>
+    <img src="${detail.image}" class='card_details' styles="height:400px" alt="${detail.name}"></img>
     </div>
     `
-    details_card.innerHTML = `
+    Card.innerHTML = `
+    <div class="container-flex mb-5"> 
      <h3 class="card-title">Category: ${detail.category}</h3>
          <h2 class="card-title">${detail.name}</h2>  
          <p class="card-text"> ${detail.description}</p>
-         <div class="card-body">
+         <div class="mt-3 h-80">
          <ul class="list-group list-group-flush">
          <p class="list-group-item" data-toggle="tooltip" data-placement="bottom" title="Date"><i class="bi bi-calendar-event-fill icon_detail"></i> ${detail.date}</p>
          <p class="list-group-item" data-toggle="tooltip" data-placement="bottom" title="Category"><i class="bi bi-bookmark-star-fill icon_detail"></i> ${detail.category}</p>
@@ -25,11 +26,10 @@ function CargarDetails() {
          <p class="list-group-item" data-toggle="tooltip" data-placement="bottom" title="Price"><strong><i class="bi bi-currency-exchange icon_detail"></i> ${detail.price}</strong></p>
          </ul>
          <div class="card-body">
-         <a class="btn background-color text-light w-100 "  data-toggle="tooltip" data-placement="bottom" title="Contact For - ${detail.name}" href="./contact.html" target='_blank'"> <i class="bi bi-send-check-fill icon_view"></i></a>
+         <a class="btn background-color text-light w-100"  data-toggle="tooltip" data-placement="bottom" title="Contact For - ${detail.name}" href="./contact.html" target='_blank'"> <i class="bi bi-send-check-fill icon_view"></i></a>
+         </div>
          </div>
      `
 }
 
-
-
-console.log(CargarDetails());
+CargarDetails();
